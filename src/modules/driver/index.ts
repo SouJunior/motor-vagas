c6c0a3a519ff4f2739a081f11205355b3d9ddad1
wolwerr <ricardo@dtmm.com.br>
2024-02-27 15:08:37 -0300

    fix: Error Firefox Scraper

--- src/modules/driver/index.ts
+++ src/modules/driver/index.ts
@@ -1,15 +1,16 @@
-import {Builder} from 'selenium-webdriver';
+import { Builder, By } from 'selenium-webdriver';
 import firefox from 'selenium-webdriver/firefox';
 
 export async function createDriver() {
-  const options = new firefox.Options()
-
+  const options = new firefox.Options();
   options.setBinary('/usr/bin/firefox');
+  options.addArguments('--headless', '--ignore-ssl-errors=yes', '--ignore-certificate-errors', '--no-sandbox');
 
-
-  options.addArguments("--headless")
-  options.addArguments('--ignore-ssl-errors=yes');
-  options.addArguments('--ignore-certificate-errors');
-
-  return new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+  try {
+    const driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+    return driver;
+  } catch (error) {
+    console.error('Erro ao criar o driver:', error);
+    throw error;
+  }
 }
