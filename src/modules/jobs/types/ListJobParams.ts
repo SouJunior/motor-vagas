f4599d5b4dd00d89e622d531d3f765c9a949ae02
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2024-09-09 19:48:02 -0300

    refactor: add filters to list jobs
    Adds filtering and sorting to list jobs route.
    - Filter strings are mapped to title, jobName, description and company fields.
    - Sorting can be set to values ASC or DESC. Default ASC

---
+++ src/modules/jobs/types/ListJobParams.ts
@@ -0,0 +1,4 @@
+export type ListJobsParams = {
+  filter?: string;
+  sort: "ASC" | "DESC";
+};
--- src/modules/jobs/useCases/listJobs/ListJobsUseCase.useCase.ts
+++ src/modules/jobs/useCases/listJobs/ListJobsUseCase.useCase.ts
@@ -1,22 +1,21 @@
 import { inject, injectable } from "tsyringe";
 import { IJobRepository } from "../../repository/IJobRepository";
 import { IResponse } from "../../interfaces/Job.interface";
+import { ListJobsParams } from "../../types/ListJobParams";
 
 @injectable()
 export class ListJobsUseCase {
-    constructor(
-        @inject("JobRepository")
-        private jobRepository: IJobRepository) {}
+  constructor(
+    @inject("JobRepository")
+    private jobRepository: IJobRepository
+  ) {}
 
-    async execute(): Promise<IResponse> {
-        
-        try {
-           const jobs = await this.jobRepository.listJobs()
-
-        return { status: 200, data: jobs, message: "Jobs listed successfully"}
-        
-        } catch (error) {
-            console.log(error.message)
-        }
+  async execute(params: ListJobsParams): Promise<IResponse> {
+    try {
+      const jobs = await this.jobRepository.listJobs(params);
+      return { status: 200, data: jobs, message: "Jobs listed successfully" };
+    } catch (error) {
+      console.log(error.message);
     }
+  }
 }
