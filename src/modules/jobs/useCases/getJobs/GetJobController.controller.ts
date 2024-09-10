bf4608f9b30d8d48c7f296aaf2909fbe1c0717c5
Gustavonobregab <gustavonobg@gmail.com>
2024-09-10 14:15:58 -0300

    GetJobsNumberFixed
     erros de ortografia removidos

--- src/modules/jobs/useCases/getJobs/GetJobController.controller.ts
+++ src/modules/jobs/useCases/getJobs/GetJobController.controller.ts
@@ -1,27 +1,32 @@
-import { Request, Response } from "express";
-import { GetJobUseCase } from "./GetJobUseCase.useCase";
-import { container } from "tsyringe";
+import { Request, Response } from 'express'
+import { GetJobUseCase } from './GetJobUseCase.useCase'
+import { container } from 'tsyringe'
 
 export class GetJobController {
-    async handle(req: Request, res: Response): Promise<Response> {
-        const {locations, keyword, description} = req.body;
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { locations, keyword, description } = req.body
 
-        const getJobUseCase = container.resolve(GetJobUseCase);
+    const getJobUseCase = container.resolve(GetJobUseCase)
 
-        try {
-            const result = await getJobUseCase.execute(locations, keyword, description);
+    try {
+      const result = await getJobUseCase.execute(
+        locations,
+        keyword,
+        description
+      )
 
-            if (result && typeof result.status === 'number' && typeof result.message === 'string') {
-                return res.status(result.status).json({ message: result.message });
-            } else {
-
-                return res.status(500).json({ message: "An unexpected error occurred" });
-            }
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: "Internal server error" });
-        }
+      if (
+        result &&
+        typeof result.status === 'number' &&
+        typeof result.message === 'string'
+      ) {
+        return res.status(result.status).json({ message: result.message })
+      } else {
+        return res.status(500).json({ message: 'An unexpected error occurred' })
+      }
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
-
-    
+  }
 }
--- src/routes/Job.routes.ts
+++ src/routes/Job.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { GetJobController } from '../modules/jobs/useCases/getJobs/GetJobController.controller'
 import { ListJobsController } from '../modules/jobs/useCases/listJobs/ListJobsController.controller'
 import { DeleteJobsController } from '../modules/jobs/useCases/deleteJobs/DeleteJobsController.controller'
-import { GetJobsNumberController } from '../modules/jobs/useCases/getJobsNumber/getJobsNumber.Controller'
+import { GetJobsNumberController } from '../modules/jobs/useCases/getJobsNumber/GetJobsNumber.controller'
 
 const jobRouter = Router()
 
